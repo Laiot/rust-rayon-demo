55fb933d08fd76c9f37de03fe619000adafb5dae
laiot <carmelo.sarta.main@gmail.com>
2022-05-26 15:40:17 +0200

    Added implementation Rust Postgresql

---
+++ rust-postgresql/src/main.rs
@@ -0,0 +1,40 @@
+use postgres::{Client, Error, NoTls};
+
+fn main() -> Result<(), Error> {
+    let mut client = Client::connect(
+        "postgresql://dboperator:operatorpass123@localhost:5243/postgres",
+        NoTls,
+    )?;
+
+    client.batch_execute(
+        "
+        CREATE TABLE IF NOT EXISTS users (
+            id              SERIAL PRIMARY KEY,
+            username        VARCHAR UNIQUE NOT NULL,
+            password        VARCHAR NOT NULL,
+            email           VARCHAR UNIQUE NOT NULL
+            )
+    ",
+    )?;
+
+    for i in 0..1000{
+        client.execute(
+            "INSERT INTO app_user (username, password, email) VALUES ($1, $2, $3)",
+            &[&format!("user{i}"), &format!("mypass"), &format!("user@test.com")],
+        )?;
+    }
+    
+    for row in client.query("SELECT id, username, password, email FROM app_user", &[])? {
+        let id: i32 = row.get(0);
+        let username: &str = row.get(1);
+        let password: &str = row.get(2);
+        let email: &str = row.get(3);
+    
+        println!(
+            "found app user: {}) {} | {} | {}",
+            id, username, password, email
+        );
+    }
+    
+    Ok(())
+}
